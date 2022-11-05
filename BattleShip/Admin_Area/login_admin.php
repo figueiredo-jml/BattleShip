900cf308b088a36c8122e57e4b84365fab7ddcbe
Filipe Rocha <ancelicus@gmail.com>
2022-11-05 20:20:49 +0000

    Parte dos admins

--- BattleShip/Admin_Area/login_admin.php
+++ BattleShip/Admin_Area/login_admin.php
@@ -1,1 +1,61 @@
-test
+<?php
+    include("../includes/db.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="caixa">
+    <div class="caixa-cabecalho">
+        <center>
+            <h1>Admin Login</h1>
+        </center>
+    </div>
+    <form action="admin_accounts.php" method="post">
+        <div class="form-group">
+            <label>Email:</label>
+            <input type="text" class="form-control" name="Email" required>
+        </div>
+        <div class="form-group">
+            <label>Password:</label>
+            <input type="password" class="form-control" name="Pass" required>
+        </div>
+
+        <div class="text-center">
+            <button name="login" value="Login" class="btn btn-primary">
+                <i class="fa fa-sign-in"></i> Log in
+            </button>
+        </div>
+    </form>
+
+</div>
+
+</body>
+</html>
+
+<?php
+if(isset($_POST['login'])){
+    $Email = $_POST['Email'];
+    $Pass = $_POST['Pass'];
+
+    $hash_password = md5($Pass);
+    $select_admin = "select * from admin_accounts where Email='$Email' AND Pass='$hash_password'";
+    $run_admin = mysqli_query($con, $select_admin);
+
+    $check_admin = mysqli_num_rows($run_admin);
+
+    if($check_admin==0){
+        // password ou email errados!!!
+        echo "
+        <div class='alert alert-danger alert-dismissible fade in'>
+            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                <strong>ERRO!</strong> Password ou email errados!.
+                </div>
+        ";
+    }
+}
+?>
